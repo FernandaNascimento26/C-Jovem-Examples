d6d7c17a30214ab7727dd9233ee836b3ed9a0e27
Fernanda Ferreira do Nascimento <ffnascimento@sfiec.org.br>
2024-12-04 16:53:29 -0300

    add novas práticas

--- Intermediário/Sincronas/JS FRONT/script1S.js
+++ Intermediário/Sincronas/JS FRONT/script1S.js
@@ -5,4 +5,35 @@ console.log(document.body.childNodes); // Exibe todos os nós filhos do <body>
 console.log(document.body.childNodes[1]); // Acessa o segundo nó filho do <body>
 console.log(document.body.childNodes[1].childNodes[1]); // Acessa o segundo nó filho dentro do nó anterior
 console.log(document.body.childNodes[1].childNodes[1].textContent); // Exibe o conteúdo de texto do nó acessado
+
+
+/*Encontrando Elementos*/
+
+
+console.log(document.getElementsByTagName('h1')); // Encontrando elementos por tag
+console.log(document.getElementsByClassName('minhaclasse')); // Encontrando elementos por classe
+console.log(document.getElementById('esteID')) //Encontrando elementos por ID
+console.log(document.querySelector('#esteID')) //Encontrando elementos por query
+
+
+/* Alterando a DOM com insertBefore*/
+let span= document.createElement("span");
+let el = document.querySelector("h1");
+let pai = el.parentNode;
+
+pai.insertBefore(span, el);
+
+
+/*Alterando a DOM com appendChild */
+
+let p= document.createElement("p");
+let el2 = document.querySelector("h1");
+let pai2 = el2.parentNode;
+
+pai2.appendChild(p);
+
+
+/**Alterando a DOM com replaceChild */
+let newHead
+
 });
