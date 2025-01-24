5276a4ad14db057f34de7e6610f863edf2746917
Fernanda Ferreira do Nascimento <ffnascimento@sfiec.org.br>
2025-01-24 15:40:32 -0300

    adicionar novos exemplos e ajustes

---
+++ Intermediário/Sincronas/9 React/react-template-ancora/src/pages/Sobre.jsx
@@ -0,0 +1,11 @@
+const Sobre = () => {
+    return (
+      <div className="page">
+        <h1>Sobre Nós</h1>
+        <p>Só pra mostrar que funciona.</p>
+      </div>
+    );
+  };
+  
+  export default Sobre;
+  
