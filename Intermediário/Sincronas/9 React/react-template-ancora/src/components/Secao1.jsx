a4258781a1ceae3c66c1ba1518ca716ce074e03f
Fernanda Ferreira do Nascimento <ffnascimento@sfiec.org.br>
2025-01-24 15:40:32 -0300

    adicionar novos exemplos e ajustes

---
+++ Intermediário/Sincronas/9 React/react-template-ancora/src/components/Secao1.jsx
@@ -0,0 +1,12 @@
+const Secao1 = () => {
+    return (
+      <section id="s1" className="section hero">
+        <h1>React: A Biblioteca Revolucionária</h1>
+        <p>Construa interfaces incríveis de forma declarativa e eficiente.</p>
+        <img src="https://i.makeagif.com/media/11-21-2022/imisbt.gif" alt="React Illustration" />
+      </section>
+    );
+  };
+  
+  export default Secao1;
+  
---
+++ Intermediário/Sincronas/9 React/react-template-ancora/src/components/Secao5.jsx
@@ -0,0 +1,15 @@
+const Secao5 = () => {
+    return (
+      <section id="s5" className="section">
+        <h2>Entre em Contato</h2>
+        <p>Se quiser aprender mais sobre React, fale conosco!</p>
+        <form>
+          <input type="email" placeholder="Digite seu e-mail" />
+          <button type="submit">Enviar</button>
+        </form>
+      </section>
+    );
+  };
+  
+  export default Secao5;
+  
