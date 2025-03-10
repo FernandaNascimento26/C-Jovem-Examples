3deab67051de9ace6a25f961ac4e60f0c0537fef
Fernanda Ferreira do Nascimento <ffnascimento@sfiec.org.br>
2025-03-10 17:31:23 -0300

    adicionar estrutura inicial do projeto TodoList com React e Vite

---
+++ Avançado/sincronas/TodoList_sem_model/Backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors'); // Importe o cors
+const app = express();
+const todoRoutes = require('./routes/todoRoutes');
+
+// Configura o CORS para permitir requisições do frontend
+app.use(cors({
+  origin: 'http://localhost:5173', // Permite apenas o frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  credentials: true, // Permite cookies e headers de autenticação
+}));
+
+app.use(express.json());
+app.use('/todos', todoRoutes);
+
+module.exports = app;
