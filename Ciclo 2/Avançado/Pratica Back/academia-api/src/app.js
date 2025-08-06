e6eaa402ce385520ba83b4751f6c9a437f73270a
Fernanda <ffnascimento@sfiec.org.br>
2025-08-06 19:26:00 -0300

    Add CORS middleware and dependencies
    - Added the 'cors' package (version 2.8.5) to enable Cross-Origin Resource Sharing in the application.
    - Included necessary files: LICENSE, README.md, and index.js for the 'cors' package.
    - Added 'object-assign' (version 4.1.1) as a dependency for object property assignment.
    - Included 'vary' (version 1.1.2) to manipulate the HTTP Vary header.
    - Created package-lock.json to lock the dependencies.
    - Updated package.json to include 'cors' as a dependency.

---
+++ Ciclo 2/Avançado/Pratica Back/academia-api/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const alunoRoutes = require('./routes/alunoRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('API Academia funcionando!');  
+
+});
+
+app.use((req, res, next) => {
+  res.removeHeader('Content-Security-Policy');
+  next();
+});
+
+app.use('api/aluno', alunoRoutes);
+
+
+module.exports = app;
